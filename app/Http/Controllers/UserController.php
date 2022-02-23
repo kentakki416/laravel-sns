3d10e5b69c0786f840a3a31403dab167e1e40a0d
kentakki416 <fujiken0814@outlook.jp>
2022-02-23 13:29:40 +0900

    SQLの改善

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function show(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load(['articles.user', 'articles.likes', 'articles.tags']);;
 
         $articles = $user->articles->sortByDesc('created_at');
 
@@ -34,7 +34,7 @@ class UserController extends Controller
 
     public function followings(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load('followings.followers');;
 
         $followings = $user->followings->sortByDesc('created_at');
 
@@ -46,7 +46,7 @@ class UserController extends Controller
     
     public function followers(string $name)
     {
-        $user = User::where('name', $name)->first();
+        $user = User::where('name', $name)->first()->load('followers.followers');;
 
         $followers = $user->followers->sortByDesc('created_at');
 
